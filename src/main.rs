d98148d92d10da0f2023a86bc35e79489d80b4fd
spencer nicol <spencern319@gmail.com>
2021-06-26 17:01:28 -0500

    Completed exercises for crash course.

--- src/main.rs
+++ src/main.rs
@@ -205,75 +205,173 @@
 
 /* Collections & Enums Exercise */
 // Silence some warnings that could distract from the exercise
-#![allow(unused_variables, unused_mut, dead_code)]
+// #![allow(unused_variables, unused_mut, dead_code)]
+//
+// enum Shot {
+//     Bullseye,
+//     Hit(f64),
+//     Miss,
+// }
+//
+// impl Shot {
+//     fn points(self) -> i32 {
+//         match self {
+//             Shot::Bullseye => 5,
+//             Shot::Hit(x) if x < 3.0 => 2,
+//             Shot::Hit(x) => 1,
+//             Shot::Miss => 0,
+//         }
+//     }
+// }
+//
+// fn main() {
+//     let arrow_coords: Vec<Coord> = get_arrow_coords(5);
+//     let mut shots: Vec<Shot> = Vec::new();
+//
+//     for coord in arrow_coords {
+//         match coord.distance_from_center() {
+//             x if x < 1.0 => shots.push(Shot::Bullseye),
+//             x if x < 5.0 => shots.push(Shot::Hit(x)),
+//             _ => shots.push(Shot::Miss),
+//         };
+//     }
+//
+//
+//     let mut total = 0;
+//     for shot in shots {
+//         total += shot.points()
+//     }
+//
+//     println!("Final point total is: {}", total);
+// }
+//
+// #[derive(Debug)]
+// struct Coord {
+//     x: f64,
+//     y: f64,
+// }
+//
+// impl Coord {
+//     fn distance_from_center(&self) -> f64 {
+//         (self.x.powf(2.0) + self.y.powf(2.0)).sqrt()
+//     }
+//     fn print_description(&self) {
+//         println!(
+//             "coord is {:.1} away, at ({:.1}, {:.1})",
+//             self.distance_from_center(),
+//             self.x,
+//             self.y);
+//     }
+//
+// }
+//
+// fn get_arrow_coords(num: u32) -> Vec<Coord> {
+//     let mut coords: Vec<Coord> = Vec::new();
+//     for _ in 0..num {
+//         let coord = Coord {
+//             x: (rand::random::<f64>() - 0.5) * 12.0,
+//             y: (rand::random::<f64>() - 0.5) * 12.0,
+//         };
+//         coords.push(coord);
+//     }
+//     coords
+// }
 
-enum Shot {
-    Bullseye,
-    Hit(f64),
-    Miss,
+/* Closures and Threads */
+#![allow(dead_code, unused_imports, unused_variables)]
+use crossbeam::channel;
+use std::thread;
+use std::time::Duration;
+
+fn expensive_sum(v: Vec<i32>) -> i32 {
+    pause_ms(500);
+    println!("Child thread: just about finished");
+    v.iter()
+        .filter(|&x| x % 2 == 0)
+        .map(|&x| x * x)
+        .sum()
 }
 
-impl Shot {
-    fn points(self) -> i32 {
-        match self {
-            Shot::Bullseye => 5,
-            Shot::Hit(x) if x < 3.0 => 2,
-            Shot::Hit(x) => 1,
-            Shot::Miss => 0,
-        }
-    }
+fn pause_ms(ms: u64) {
+    thread::sleep(Duration::from_millis(ms));
 }
 
 fn main() {
-    let arrow_coords: Vec<Coord> = get_arrow_coords(5);
-    let mut shots: Vec<Shot> = Vec::new();
+    // let my_vector = vec![2, 5, 1, 0, 4, 3];
+    // let handle = thread::spawn(move || {
+    //     expensive_sum(my_vector)
+    // });
+    //
+    // for letter in vec!["a", "b", "c", "d", "e", "f"] {
+    //     println!("Main thread: Letter {}", letter);
+    //     pause_ms(200);
+    // }
+    //
+    // let sum = handle.join().unwrap();
+    // println!("The child thread's expensive sum is {}", sum);
+    //
+    // let (tx, rx) = channel::unbounded();
+    // // Cloning a channel makes another variable connected to that end of the channel so that you can
+    // // send it to another thread.
+    // let tx2 = tx.clone();
+    // let handle_a = thread::spawn(move || {
+    //     pause_ms(200);
+    //     tx2.send("Thread A: 1").unwrap();
+    //     pause_ms(0);
+    //     tx2.send("Thread A: 2").unwrap();
+    // });
+    // pause_ms(0); // Make sure Thread A has time to get going before we spawn Thread B
+    // let handle_b = thread::spawn(move || {
+    //     pause_ms(0);
+    //     tx.send("Thread B: 1").unwrap();
+    //     pause_ms(0);
+    //     tx.send("Thread B: 2").unwrap();
+    // });
+    // // Using a Receiver channel as an iterator is a convenient way to get values until the channel
+    // // gets closed.  A Receiver channel is automatically closed once all Sender channels have been
+    // // closed.  Both our threads automatically close their Sender channels when they exit and the
+    // // destructors for the channels get automatically called.
+    // for msg in rx {
+    //     println!("Main thread: Received {}", msg);
+    // }
+    // // Join the child threads for good hygiene.
+    // handle_a.join().unwrap();
+    // handle_b.join().unwrap();
 
-    for coord in arrow_coords {
-        match coord.distance_from_center() {
-            x if x < 1.0 => shots.push(Shot::Bullseye),
-            x if x < 5.0 => shots.push(Shot::Hit(x)),
-            _ => shots.push(Shot::Miss),
+    let (tx, rx) = channel::unbounded();
+    let rx2 = rx.clone();
+    let handler_1 = thread::spawn(move || {
+        for msg in rx {
+            pause_ms(50);
+            println!("Handler 1: Received {}", msg);
+        }
+    });
+    let handler_2 = thread::spawn(move || {
+        for msg in rx2 {
+            pause_ms(50);
+            println!("Handler 2: Received {}", msg);
+        }
+    });
+
+    let vector = vec!["a", "b", "c", "d", "e", "f"];
+
+    for i in 0..=5 {
+        if i > 3 {
+            tx.send(vector[i]).unwrap()
+        } else {
+            println!("{}", vector[i])
         };
     }
+    // Join the child threads for good hygiene.
+    drop(tx);
+    handler_1.join().unwrap();
+    handler_2.join().unwrap();
 
 
-    let mut total = 0;
-    for shot in shots {
-        total += shot.points()
-    }
-
-    println!("Final point total is: {}", total);
+    // Challenge: Make two child threads and give them each a receiving end to a channel.  From the
+    // main thread loop through several values and print each out and then send it to the channel.
+    // On the child threads print out the values you receive. Close the sending side in the main
+    // thread by calling `drop(tx)` (assuming you named your sender channel variable `tx`).  Join
+    // the child threads.
+    println!("Main thread: Exiting.")
 }
-
-#[derive(Debug)]
-struct Coord {
-    x: f64,
-    y: f64,
-}
-
-impl Coord {
-    fn distance_from_center(&self) -> f64 {
-        (self.x.powf(2.0) + self.y.powf(2.0)).sqrt()
-    }
-    fn print_description(&self) {
-        println!(
-            "coord is {:.1} away, at ({:.1}, {:.1})",
-            self.distance_from_center(),
-            self.x,
-            self.y);
-    }
-
-}
-
-fn get_arrow_coords(num: u32) -> Vec<Coord> {
-    let mut coords: Vec<Coord> = Vec::new();
-    for _ in 0..num {
-        let coord = Coord {
-            x: (rand::random::<f64>() - 0.5) * 12.0,
-            y: (rand::random::<f64>() - 0.5) * 12.0,
-        };
-        coords.push(coord);
-    }
-    coords
-}
-
