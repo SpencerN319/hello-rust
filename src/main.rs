5e97e956015b4aacb77184e9b5423dd057386f33
spencer nicol <spencern319@gmail.com>
2021-06-26 09:27:10 -0500

    finished control flow exercise.

--- src/main.rs
+++ src/main.rs
@@ -28,28 +28,78 @@
 //     x * y * z
 // }
 
-#![allow(dead_code, unused_variables)]
-use hello_rust::{
-    print_difference,
-    print_array,
-    ding,
-    on_off,
-    print_distance,
-};
+/* Simple Types Exercise */
+// use hello_rust::{
+//     print_difference,
+//     print_array,
+//     ding,
+//     on_off,
+//     print_distance,
+// };
+//
+//
+// fn main() {
+//     let coords: (f32, f32) = (6.3, 15.0);
+//     let coords_arr: [f32; 2] = [coords.0, coords.1];
+//     print_difference(coords.0, coords.1);
+//     print_array(coords_arr);
+//
+//     let series = [1, 1, 2, 3, 5, 8, 13];
+//     ding(series[6]);
+//
+//     let mess = ([3, 2], 3.14, [(false, -3), (true, -100)], 5, "candy");
+//     on_off(mess.2[1].0);
+//
+//     print_distance(coords);
+// }
 
+/* Control Flow and Strings */
+// Silence some warnings so they don't distract from the exercise.
+#![allow(dead_code, unused_mut, unused_variables)]
 
 fn main() {
-    let coords: (f32, f32) = (6.3, 15.0);
-    let coords_arr: [f32; 2] = [coords.0, coords.1];
-    print_difference(coords.0, coords.1);
-    print_array(coords_arr);
+    let args: Vec<String> = std::env::args().skip(1).collect();
 
-    let series = [1, 1, 2, 3, 5, 8, 13];
-    ding(series[6]);
-
-    let mess = ([3, 2], 3.14, [(false, -3), (true, -100)], 5, "candy");
-    on_off(mess.2[1].0);
-
-    print_distance(coords);
+    for arg in args {
+        if arg == "sum" {
+            sum()
+        } else if arg == "double" {
+                double()
+        } else {
+            count(arg)
+        };
+    }
 }
 
+fn sum() {
+    let mut sum = 0;
+    for num in 7..=23 {
+        sum += num
+    }
+
+    println!("The sum is {}", sum);
+}
+
+fn double() {
+    let mut count = 0;
+    let mut x = 1;
+    while x < 500 {
+        x *= 2;
+        count += 1;
+    }
+
+    println!("You can double x {} times until x is larger than 500", count);
+}
+
+fn count(arg: String) {
+    let mut count = 0;
+    loop {
+        println!("{}", arg);
+        count += 1;
+        if count == 8 {
+            break;
+        }
+    };
+
+    println!();
+}
